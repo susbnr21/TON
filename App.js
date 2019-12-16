eb4747c14ddbcdc7a541c38cf98cb442bd4e83ee
aaa <sushilchoru@Sushils-MacBook-Pro.local>
2019-12-16 12:23:57 +0545

    Added navigator

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './app/app.navigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This app is going to be Employee Attendance Checker.</Text>
-      <Text>Lets Gets Started!!!</Text>
-    </View>
+    <AppNavigator/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/app.navigator.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { DrawerNavigator, StackRouter } from 'react-navigation';
+
+const RootConfig = {}
+const AppNavigator = DrawerNavigator({}, RouteConfig);
+
+export default AppNavigator;
